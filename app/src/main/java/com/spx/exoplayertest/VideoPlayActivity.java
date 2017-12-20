cf3b3c731f07b4f0137b9c85361e5c97151cb351
shaopengxiang <shaopengxiang@sogou-inc>
2017-12-20 19:06:16 +0800

    modify

--- app/src/main/java/com/spx/exoplayertest/VideoPlayActivity.java
+++ app/src/main/java/com/spx/exoplayertest/VideoPlayActivity.java
@@ -43,9 +43,7 @@ public class VideoPlayActivity extends Activity {
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
